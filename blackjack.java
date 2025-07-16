b8556a896544997c75bda12a7817e0081e77d491
Shriya-vis <shriya.visu@gmail.com>
2025-07-15 22:44:34 -0700

    Update blackjack.java

--- blackjack.java
+++ blackjack.java
@@ -1,2 +1,12 @@
-#include arraylist
 public class blackjack{
+  public static void main(String[] args){
+    int[] nums = {18,19,26,16,20,23,13,30,15,28};
+    int max = 0 ;
+    for (int i = 0; i < nums.length;i++){
+      if (nums[i] > max && nums[i]<21){
+        max = nums[i] ;
+      }
+    }
+    System.out.println(max);
+  }
+}
