55ba42cbf790231b612010fa83b1285fdff45035
Shriya-vis <shriya.visu@gmail.com>
2025-07-17 19:44:57 -0700

    Delete Hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello{
-    public static void main(String[] args){
-        int A = 5;
-        int B = 7;
-        int height = 10;
-        double result = (0.5)*height*(A+B);
-        System.out.println(result);
-    }
-}
