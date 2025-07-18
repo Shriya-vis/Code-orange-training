e27ced828776a9e2b4dc7eb634d70195b4e5cca5
Shriya-vis <shriya.visu@gmail.com>
2025-07-17 18:30:04 -0700

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,9 @@
+public class Area{
+    public static void main(String[] args){
+        int A = 5;
+        int B = 7;
+        int height = 10;
+        double result = (0.5)*height*(A+B);
+        System.out.println(result);
+    }
+}
